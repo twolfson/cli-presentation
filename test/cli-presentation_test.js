a562aa5ad478f045d9f98fc81bc258d4ce2b5782
Todd Wolfson <todd@twolfson.com>
2013-12-06 04:11:26 -0800

    Setting up and tearing down temporary directory

--- test/cli-presentation_test.js
+++ test/cli-presentation_test.js
@@ -1,8 +1,24 @@
-var cli_presentation = require('../');
+var exec = require('child_process').exec;
+var TempDir = require('temporary').Dir;
+var wrench = require('wrench');
 
-describe('cli-presentation', function () {
+describe('A cli-presentation', function () {
+  // Create temporary directory to test within
   before(function () {
+    this.tmpDir = new TempDir();
+    this.tmpPath = this.tmpDir.path;
+  });
+  after(function (done) {
+    // DEV: We cannot use temporary to clean up since it won't be empty
+    wrench.rmdirRecursive(this.tmpPath, done);
+  });
 
+  // Copy over our temp files
+  before(function (done) {
+    console.log(this.tmpPath);
+    wrench.copyDirRecursive(__dirname + '/test-files/multi', this.tmpPath, {
+      forceDelete: true
+    }, done);
   });
 
   it('', function () {
